dd611dbb792a333c04812e6fea4d896e8a4c5e03
Farrel Putra <70509726+thundergaming349@users.noreply.github.com>
2026-05-28 00:30:35 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "tugaspkkbe-production.up.railway.app/api";
+const API_BASE_URL = "https://tugaspkkbe-production.up.railway.app/api";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
